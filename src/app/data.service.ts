f9a9bea4a51c7667fbbaf46452952430a7bad8a9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-13 18:51:15 +0300

    Step #3 (#4)
    Sets up a simple data service with some test data from the AngularConnect schedule.

---
+++ src/app/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+export interface Talk {
+  text: string;
+  speaker?: string;
+  tags?: string[];
+  image?: string;
+}
+
+export interface Track {
+  title: string;
+  talks: Talk[];
+}
+
+export interface Board {
+  title: string;
+  tracks: Track[];
+}
+
+@Injectable({providedIn: 'root'})
+export class BoardService {
+  private _boards: Board[] = require('./data.json');
+
+  getBoards(): Board[] {
+    return this._boards;
+  }
+}
